b96d66e113293d256804928d6ccba4f53a8a44c8
semba77 <86146568+semba77@users.noreply.github.com>
2023-11-15 20:36:03 +0100

    Add files via upload
    hotová verze

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import re
 
 app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
-
+nicknames = set()
+zprava=[]
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('prvni_stranka.html'), 200
+    return render_template('prvni_stranka.html', zprava=zprava), 200
+
+@app.route('/api/check-nickname', methods=['GET'])
+def check_nickname():
+    nick = request.args.get('nick')
+    print(nick)
+    print(nicknames)
+    if nick in nicknames:
+        print("7777777")
+        return jsonify({"exists": True})
+    else:
+        print("9999999")
+        return jsonify({"exists": False})
 
 
 @app.route('/registrace', methods=['GET', 'POST'])
 def druha_stranka():
-    return render_template('druha_stranka.html', zprava="..."), 200
-
-@app.route('/odeslat', methods=['GET', 'POST'])
-def odeslat():
-    data1 = request.form['je_plavec']
-    data2 = request.form['nick']
-    data3 = request.form['kanoe_kamarad']
-    if data1 is True:
-        if re.search("[a-zA-Z]{2,20}",data2):
-            if re.search("[a-zA-Z]{2,20}",data3):
-                formular = {
-                    "nick": data2,
-                    "kamarad": data3
-                }
+    if request.method == 'GET':
+        return render_template('druha_stranka.html', zprava=zprava), 200
+    if request.method == 'POST':
+        data1 = request.form.get('je_plavec')
+        data2 = request.form.get('nick')
+        data3 = request.form.get('kanoe_kamarad')
+        if data1 == "True":
+            print("111111")
+            if re.search("[a-zA-Z0-9]{2,20}",data2):
+                print("2222222")
+                if re.search("[a-zA-Z0-9]{2,20}",data3):
+                    print("3333333")
+                    zprava.append([data2, data3])
+                    nicknames.add(data2)
+                    return render_template('prvni_stranka.html', zprava=zprava), 200
+                else:
+                    zprava.append(data2)
+                    nicknames.add(data2)
+                    return render_template('prvni_stranka.html', zprava=zprava), 200
             else:
-                formular = {
-                    "nick": data2,
-                }
+                print("----111111")
+                return render_template('druha_stranka.html'), 400
         else:
-            return "kod:400"
-    else:
-        return "kod:400"
+            print("--111111")
+            return render_template('druha_stranka.html'), 400
 
 
 if __name__ == '__main__':
